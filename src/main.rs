e4ee36db0eed1054f7c3eb452cad9df88175c5df
vdbewout <vdbewout@gmail.com>
2023-04-11 15:18:15 +0200

    crates

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use tracing::info;
+use tracing_subscriber::{fmt, prelude::*, EnvFilter};
+
+#[actix::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .init();
+
+    info!("Hello, world!");
 }
